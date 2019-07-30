1f771d8559b8d89a3d9dddbdf786ce8a1659a9d3
Tarun <jaita03@ca.com>
2019-07-30 17:43:23 +0530

    Implementing this with DP

--- tjexamplescodes/src/com/test/org/Dream11/NoOfPathInMatrix.java
+++ tjexamplescodes/src/com/test/org/Dream11/NoOfPathInMatrix.java
@@ -8,7 +8,7 @@ public class NoOfPathInMatrix {
 /*		Tech Discussions
 		1 1 0 1
 		1 1 1 1
-		1 0 1 1
+		0 1 1 1
 */
 int m,n;
 m = 3; n = 4;
@@ -22,15 +22,17 @@ arr[1][0]=1;
 arr[1][1]=1;
 arr[1][2]=1;
 arr[1][3]=1;
-arr[2][0]=1;
-arr[2][1]=0;
+arr[2][0]=0;
+arr[2][1]=1;
 arr[2][2]=1;
 arr[2][3]=1;
-		int x =	numOfPaths(arr, 0, 0, m, n);
-	System.out.println(x);
+
+		int x =	numOfPathsRecursion(arr, 0, 0, m, n);
+		int y = numOfPathsDP(arr, m,n);
+	System.out.println(x + " " + y);
 	}
 
-	public static int numOfPaths(int[][] arr, int i, int j, int m, int n)
+	public static int numOfPathsRecursion(int[][] arr, int i, int j, int m, int n)
 	{
 		if(i == m || j == n)
 			return 0;
@@ -43,6 +45,34 @@ arr[2][3]=1;
 				return 1;
 			return 0;
 		}
-		return numOfPaths(arr, i, j + 1, m, n) + numOfPaths(arr, i + 1, j, m, n);
+		return numOfPathsRecursion(arr, i, j + 1, m, n) + numOfPathsRecursion(arr, i + 1, j, m, n);
+	}
+	
+	public static int numOfPathsDP(int[][] arr, int m, int n)
+	{
+		int [][] temp = new int [m][n];
+		
+		for(int x = 0; x < m; x++ )
+		{
+			for(int y = 0; y < n; y++ )
+			{
+				if(x == 0 && y == 0)
+				{
+					temp[0][0] = 1;
+					continue;
+				}
+				
+				if (arr[x][y] == 1)
+				{
+					temp[x][y] = ((x-1) >= 0 ? temp[x-1][y] : 0) + ((y-1) >= 0 ? temp[x][y-1] : 0);
+				}
+				else
+				{
+					temp[x][y] = 0;
+				}
+			}	
+		}
+		return temp[m-1][n-1];
+		
 	}
 }
